2769f1222e035c099b6ed198b6ab3b7b7f59bbf2
markbanaria <markbanaria@gmail.com>
2017-07-26 20:03:19 +0800

    change foot and phone number

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,17 +1,17 @@
-var $document = $(document);
-var $window = $(window);
-var $navbarReylable = $('.navbar-reylable');
-var $homePage = $('.home-page');
+// var $document = $(document);
+// var $window = $(window);
+// var $navbarReylable = $('.navbar-reylable');
+// var $homePage = $('.home-page');
 
-var checkTop = function() {
-	return ($window.scrollTop > 0) 
-} 
+// var checkTop = function() {
+// 	return ($window.scrollTop > 0) 
+// } 
 
-$document.ready(function() {
-	$window.on('scroll', function() {
-		if(checkTop) {
-			$navbarReylable.addClass('sticky');
-			//$homePage.css('margin-top', $navbarReylable.height());
-		}
-	})
-});
+// $document.ready(function() {
+// 	$window.on('scroll', function() {
+// 		if(checkTop) {
+// 			$navbarReylable.addClass('sticky');
+// 			$homePage.css('margin-top', $navbarReylable.height());
+// 		}
+// 	})
+// });
